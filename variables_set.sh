ed7c1e27b7afadc703b5ce2e4774b3b19ab8a881
Alexey Medvecky <alexey.medvecky@gmail.com>
2019-05-18 20:33:36 +0300

    Delete variables_set.sh

--- variables_set.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export REDISCLOUD_URL="redis://rediscloud:NYGccegqxrCps5a9O5S46tooNfOBbSWX@redis-10107.c1.eu-west-1-3.ec2.cloud.redislabs.com:10107"
-
-echo ${REDISCLOUD_URL}
-
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/alexeymedvecky/projects/cached_translations-f927008f4f77.json"
-
-echo ${GOOGLE_APPLICATION_CREDENTIALS}
