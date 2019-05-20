bb6147241560186aa22e5a2dfaa4680e824c166d
medvecky <alexey.medvecky@gmail.com>
2019-05-20 23:13:39 +0300

    normal CLI client

--- cached_translation_client.py
+++ cached_translation_client.py
@@ -6,23 +6,32 @@ import grpc
 import cached_translation_pb2
 import cached_translation_pb2_grpc
 
+import argparse
+
+parser = argparse.ArgumentParser(description="CLI for cached translation")
+parser.add_argument("--source", metavar="Source", type=str)
+parser.add_argument("--to", metavar="Target", type=str, required=True)
+parser.add_argument("strings", metavar="Text", type=str, nargs="+")
+
+args = parser.parse_args()
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = cached_translation_pb2_grpc.CachedTranslationStub(channel)
 
-        texts = ["Hello world guys"]
-        source_language = "en"
-        target_language="ru"
+        texts = args.strings
+        source_language = args.source
+        target_language = args.to
 
         response = stub.GetTranslations(cached_translation_pb2.TranslationRequest(
             texts=texts,
             targetLanguage=target_language,
             sourceLanguage=source_language))
 
-
-    print(response.translations[0].translatedText)
-    print(response.translations[0].input)
+    for translation in response.translations:
+        print(translation.translatedText)
+        print(translation.detectedSourceLanguage)
+        print(translation.input)
 
 
 if __name__ == '__main__':
